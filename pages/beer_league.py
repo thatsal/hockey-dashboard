82ee70fe85ba26144255216493ad0a96e93a8d97
thatsal <thatsal.project@gmail.com>
2026-03-19 16:48:30 -0700

    Update beer_league.py

--- pages/beer_league.py
+++ pages/beer_league.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
 
+st.title("🍺 Beer League")
+
+data = {
+    "Player": ["Alex", "Chris", "Jordan"],
+    "Goals": [10, 7, 5],
+    "Assists": [8, 6, 9]
+}
+
+df = pd.DataFrame(data)
+df["Points"] = df["Goals"] + df["Assists"]
+
+st.dataframe(df)
+
+st.bar_chart(df.set_index("Player")["Points"])
